ab78ea55890ffd01369bbad0061fdff073a1e05c
Asriel550 <raelsalmador@gmail.com>
2025-10-23 07:50:12 +0000

    Create script.py

---
+++ script.py
@@ -0,0 +1,101 @@
+[{
+	"resource": "/workspaces/Auto-Narrador-IA/script.py",
+	"owner": "pylance7",
+	"code": {
+		"value": "reportMissingImports",
+		"target": {
+			"$mid": 1,
+			"path": "/microsoft/pylance-release/blob/main/docs/diagnostics/reportMissingImports.md",
+			"scheme": "https",
+			"authority": "github.com"
+		}
+	},
+	"severity": 4,
+	"message": "Import \"google.generativeai\" could not be resolved",
+	"source": "Pylance",
+	"startLineNumber": 2,
+	"startColumn": 8,
+	"endLineNumber": 2,
+	"endColumn": 27,
+	"origin": "extHost2"
+},{
+	"resource": "/workspaces/Auto-Narrador-IA/script.py",
+	"owner": "pylance7",
+	"code": {
+		"value": "reportMissingImports",
+		"target": {
+			"$mid": 1,
+			"path": "/microsoft/pylance-release/blob/main/docs/diagnostics/reportMissingImports.md",
+			"scheme": "https",
+			"authority": "github.com"
+		}
+	},
+	"severity": 4,
+	"message": "Import \"google.genai\" could not be resolved",
+	"source": "Pylance",
+	"startLineNumber": 4,
+	"startColumn": 6,
+	"endLineNumber": 4,
+	"endColumn": 18,
+	"origin": "extHost2"
+},{
+	"resource": "/workspaces/Auto-Narrador-IA/script.py",
+	"owner": "pylance7",
+	"code": {
+		"value": "reportMissingImports",
+		"target": {
+			"$mid": 1,
+			"path": "/microsoft/pylance-release/blob/main/docs/diagnostics/reportMissingImports.md",
+			"scheme": "https",
+			"authority": "github.com"
+		}
+	},
+	"severity": 4,
+	"message": "Import \"gtts\" could not be resolved",
+	"source": "Pylance",
+	"startLineNumber": 5,
+	"startColumn": 6,
+	"endLineNumber": 5,
+	"endColumn": 10,
+	"origin": "extHost2"
+},{
+	"resource": "/workspaces/Auto-Narrador-IA/script.py",
+	"owner": "pylance7",
+	"code": {
+		"value": "reportMissingImports",
+		"target": {
+			"$mid": 1,
+			"path": "/microsoft/pylance-release/blob/main/docs/diagnostics/reportMissingImports.md",
+			"scheme": "https",
+			"authority": "github.com"
+		}
+	},
+	"severity": 4,
+	"message": "Import \"faiss\" could not be resolved",
+	"source": "Pylance",
+	"startLineNumber": 13,
+	"startColumn": 12,
+	"endLineNumber": 13,
+	"endColumn": 17,
+	"origin": "extHost2"
+},{
+	"resource": "/workspaces/Auto-Narrador-IA/script.py",
+	"owner": "pylance7",
+	"code": {
+		"value": "reportMissingImports",
+		"target": {
+			"$mid": 1,
+			"path": "/microsoft/pylance-release/blob/main/docs/diagnostics/reportMissingImports.md",
+			"scheme": "https",
+			"authority": "github.com"
+		}
+	},
+	"severity": 4,
+	"message": "Import \"whisper\" could not be resolved",
+	"source": "Pylance",
+	"startLineNumber": 19,
+	"startColumn": 12,
+	"endLineNumber": 19,
+	"endColumn": 19,
+	"origin": "extHost2"
+}]
